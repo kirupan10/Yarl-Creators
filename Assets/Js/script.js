1edc1f8ff75f15dca8c73b1f3a8c6dbf7f9feef5
Mohamed Shukry <123892337+mhdshukry@users.noreply.github.com>
2025-03-30 14:17:13 +0530

    Aboutus and updated

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -12,20 +12,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-  window.addEventListener("scroll", () => {
+window.addEventListener("scroll", () => {
     if (window.scrollY > 300) {
         scrollToTopBtn.classList.add("show");
     } else {
         scrollToTopBtn.classList.remove("show");
     }
-  });
+});
 
-  scrollToTopBtn.addEventListener("click", () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-  });
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
 
