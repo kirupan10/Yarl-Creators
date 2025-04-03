07f9ed6d675bb7b15dfa93048587856f44ac2fa6
Kirupan Inpathas <114853337+kirupan10@users.noreply.github.com>
2025-04-03 20:49:33 +0530

    layout hero section

--- YarlCreators/resources/views/layout.blade.php
+++ YarlCreators/resources/views/layout.blade.php
@@ -27,6 +27,54 @@
 
 <body>
 
+    <section class="hero">
+        <div class="top-icons">
+            <!-- Top Icons -->
+            <a href="/" class="icon home"><i class="fas fa-home"></i></a>
+            <a href="#" class="icon cart"><i class="fas fa-shopping-cart"></i><span class="badge">1</span></a>
+            <a href="#" class="icon notification"><i class="fas fa-bell"></i><span class="badge">3</span></a>
+
+
+            @if (Auth::check())
+                <!-- User is logged in, show user profile or logout icon -->
+                <a href="{{ route('profile') }}" class="icon login"><i class="fas fa-user"></i></a>
+                <a href="{{ route('logout') }}" class="icon logout"><i class="fas fa-sign-out-alt"></i></a>
+            @else
+                <!-- User is not logged in, show login icon -->
+                <a href="{{ route('login') }}" class="icon login"><i class="fas fa-sign-in-alt"></i></a>
+            @endif
+
+        </div>
+
+        <div class="logo">
+            <img src="{{ asset('images/logo.png') }}" alt="Yarl Creators Logo">
+        </div>
+        <div class="hero-text">
+            <h1 class="big-text">YARL</h1>
+            <span class="small-text">CREATORS</span>
+        </div>
+        <p class="hero-description">
+            Yarl Creators is a digital media production company.<br>
+            We specialize in video production and digital marketing.<br>
+            Powered by a network of creative and innovative storytellers.
+        </p>
+
+        <!-- Social Icons -->
+        <div class="social-icons">
+            <a href="https://www.facebook.com/p/Yarl-Creators-100083580871638/" target="_blank"><i
+                    class="fab fa-facebook-f"></i></a>
+            <a href="https://www.instagram.com/yarl_creators/" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.tiktok.com/@yarl_creators" target="_blank"><i class="fab fa-tiktok"></i></a>
+            <a href="https://www.youtube.com/channel/UC05DrDx4pGPX7_zVvxkdqJg" target="_blank"><i
+                    class="fab fa-youtube"></i></a>
+        </div>
+
+        <div class="button-container">
+            <button class="book-btn">Book Now <i class="fas fa-arrow-right"></i></button>
+        </div>
+
+    </section>
+
     <div class="container mt-4">
         @yield('content')
     </div>
