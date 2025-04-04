ec98f1e92671fac66556dd25d0e07a593b0bba6a
Kirupan Inpathas <144366290+hiruththikan@users.noreply.github.com>
2025-04-04 10:49:35 +0530

    blog view

--- YarlCreators/resources/views/blogpage.blade.php
+++ YarlCreators/resources/views/blogpage.blade.php
@@ -163,19 +163,19 @@
             <span>Blog</span>
         </a>
 
-        <a href="shop.html" class="nav-item" data-page="shop">
+        <a href="{{route('products')}}" class="nav-item" data-page="shop">
             <i class="fas fa-shopping-bag"></i>
             <span>Shop</span>
         </a>
-        <a href="index.html" class="nav-item" data-page="home">
+        <a href="/" class="nav-item" data-page="home">
             <i class="fas fa-home"></i>
             <span>Home</span>
         </a>
-        <a href="aboutus.html" class="nav-item" data-page="about">
+        <a href="{{route('about')}}" class="nav-item" data-page="about">
             <i class="fas fa-users"></i>
             <span>About</span>
         </a>
-        <a href="contact.html" class="nav-item" data-page="contact">
+        <a href="{{route('contact')}}" class="nav-item" data-page="contact">
             <i class="fas fa-envelope"></i>
             <span>Contact</span>
         </a>
