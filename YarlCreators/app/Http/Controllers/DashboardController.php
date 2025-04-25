12c64c11e6e62a6ac7dfd7452633a2859608032c
kirupan10 <ikirupan@gmail.com>
2025-04-25 10:47:42 +0530

    Optimising

--- YarlCreators/app/Http/Controllers/DashboardController.php
+++ YarlCreators/app/Http/Controllers/DashboardController.php
@@ -17,4 +17,15 @@ class DashboardController extends Controller
 
     return view('admin.dashboard', compact('user'));
 }
+public function feedback_view(){
+
+    if (!auth()->check()) {
+        return redirect()->route('login');
+    }
+
+    // Or safely access the user
+    $user = auth()->user();
+
+    return view('admin.feedback', compact('user'));
+}
 }
