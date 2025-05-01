fcb1ef9152fb360a013a6fc76471be9ba3ae296e
kirupan10 <ikirupan@gmail.com>
2025-05-01 13:58:16 +0530

    asset img fixed

--- YarlCreators/resources/views/Admin/create_blog.blade.php
+++ YarlCreators/resources/views/Admin/create_blog.blade.php
@@ -7,7 +7,7 @@
     <title>Create New Blog | Yarl Creators</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
-    <link rel="icon" href="../Assets/images/logo.png" type="image/png" />
+    <link rel="icon" type="image/png" href="{{ asset('Assets/images/logo.png') }}" />
     <link rel="stylesheet" href=" {{ asset('admin_assets/css/create_blog.css') }}" />
 </head>
 
