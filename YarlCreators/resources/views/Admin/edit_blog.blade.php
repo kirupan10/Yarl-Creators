a15a19ac75e64129dfead2201ee87d1d45f3b200
Mohamed Shukry <123892337+mhdshukry@users.noreply.github.com>
2025-05-01 13:31:37 +0530

    Update edit_blog.blade.php

--- YarlCreators/resources/views/Admin/edit_blog.blade.php
+++ YarlCreators/resources/views/Admin/edit_blog.blade.php
@@ -86,51 +86,7 @@
 
     </main>
 
-    <script>
-        const existingGallery = [
-            "../Assets/images/bride.jpg",
-            "../Assets/images/high.jpg",
-            "../Assets/images/rece.jpg"
-        ];
-
-        function loadExistingGallery() {
-            const galleryPreview = document.getElementById('galleryPreview');
-            existingGallery.forEach(img => {
-                const imageElement = document.createElement('img');
-                imageElement.src = img;
-                galleryPreview.appendChild(imageElement);
-            });
-        }
-
-        function previewMainImage(event) {
-            const reader = new FileReader();
-            reader.onload = function () {
-                document.getElementById('mainImagePreview').src = reader.result;
-            }
-            reader.readAsDataURL(event.target.files[0]);
-        }
-
-        function previewGallery(event) {
-            const galleryPreview = document.getElementById('galleryPreview');
-            Array.from(event.target.files).forEach(file => {
-                const reader = new FileReader();
-                reader.onload = function () {
-                    const img = document.createElement('img');
-                    img.src = reader.result;
-                    galleryPreview.appendChild(img);
-                }
-                reader.readAsDataURL(file);
-            });
-        }
-
-        document.getElementById('editBlogForm').addEventListener('submit', function (e) {
-            e.preventDefault();
-            alert('Blog updated successfully!');
-            window.location.href = 'blog-management.html';
-        });
-
-        window.onload = loadExistingGallery;
-    </script>
+    <script src="./admin_assets/js/edit_blog.js"></script>
 
 </body>
 
