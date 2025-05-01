101137a4cb3c32e831eba32cfb5b9d6fcacf6905
kirupan10 <ikirupan@gmail.com>
2025-05-01 13:54:40 +0530

    Update report_management.blade.php

--- YarlCreators/resources/views/Admin/report_management.blade.php
+++ YarlCreators/resources/views/Admin/report_management.blade.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
     <link rel="icon" href="../Assets/images/logo.png" />
-    <link rel="stylesheet" href=" {{ asset('admin_assets/css/eport_managemnt.css') }}" />
+    <link rel="stylesheet" href=" {{ asset('admin_assets/css/report_managemnt.css') }}" />
 </head>
 
 <body>
